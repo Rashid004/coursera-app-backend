fad8a021d4d56a09f44571e4194d517552971986
Ansari Rashid <117913799+Rashid004@users.noreply.github.com>
2025-03-01 18:04:41 +0530

    Update user.js
    add user req in userRoute

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,50 @@
 const { Router } = require("express");
-
+const { z } = require("zod");
 const userRouter = Router();
 
 const { userModel } = require("../db");
 
-userRouter.post("/signup", (req, res) => {
+userRouter.post("/signup", async (req, res) => {
+  const requireBody = z.object({
+    email: z.string().min(3).max(100).regex(/[@]/).email(),
+    password: z
+      .string()
+      .min(6)
+      .max(30)
+      .regex(/[a-z]/)
+      .regex(/[A-Z]/)
+      .regex(/[0-9]/)
+      .regex(/[^a-zA-Z0-9]/),
+    firstName: z.string().min(1),
+    lastName: z.string().min(1),
+  });
+
+  const pasrsedBodyWithSuccess = requireBody.safeParse(req.body);
+
+  if (!pasrsedBodyWithSuccess.success) {
+    res.json({
+      message: pasrsedBodyWithSuccess.success,
+      error: pasrsedBodyWithSuccess.error,
+    });
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+
+  try {
+    await userModel.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(403).json({ message: "Credential is Incorrect" });
+  }
+
   res.json({ message: "signup endpoint" });
 });
 
